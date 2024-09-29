b69d7f46f7341bde42a162a58a6c777b2e316c3a
tamarElimelech <tamar21487@gmail.com>
2024-09-29 20:38:15 +0300

    Add files via upload

---
+++ events/events/Event.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace events
+{
+    class Event
+    {
+        static int id;
+        private string title;
+        private DateTime start;
+        static public int cnt = 0;
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+
+
+        public Event()
+        {
+         
+            Id=id++;
+            Title = null;
+            Start = DateTime.Now;
+
+        }
+        public Event(string title)
+        {
+            Id = id++;
+            Title = title;
+            Start = DateTime.Now;
+        }
+
+     
+    }
+}
+
+       
+
---
+++ events/events/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace events
+{
+    class Program
+    {
+       static List<Event> eventsl = new List<Event>();
+       static public List<Event> GET()
+        {
+            return eventsl;
+        }
+
+        static public void POST(Event e)
+        {
+            eventsl.Add(e);
+        }
+
+        static public void PUT(int i, string t)
+        {
+            Event ev = eventsl.Where(e => e.Id == i).First();
+            ev.Title = t;
+        }
+        static public void DELETE(int id)
+        {
+            Event ev = eventsl.Where(e => e.Id == id).First();
+            eventsl.Remove(ev);
+        }
+
+
+    static void Main(string[] args)
+        {
+            Event e1 = new Event("evening");
+            Event e2 = new Event("morning");
+            Event e3 = new Event("afternoon");
+
+            POST(e1);
+            POST(e2);
+            POST(e3);
+            List<Event> events2 = new List<Event>();
+            events2=GET();
+            foreach(var e in events2)
+            {
+                Console.WriteLine("id:"+e.Id+" title: "+e.Title);
+            }
+            PUT(0, "UPDATE");
+            DELETE(1);
+            Console.WriteLine("events after");
+            foreach (var e in eventsl)
+            {
+                Console.WriteLine("id:" + e.Id + " title: " + e.Title);
+            }
+            Console.ReadLine();
+        }
+    }
+}
